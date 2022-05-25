b468d6b3594baff694d7059f0eed22f9e26df376
Mihail Mishkin <mihail.mishkin89@gmail.com>
2022-05-25 19:00:09 +0300

    Внес мелкие правки в стили

--- src/components/Friends/FriendListItem.jsx
+++ src/components/Friends/FriendListItem.jsx
@@ -4,7 +4,7 @@ import s from './FriendList.module.css'
 const FriendListItem = ({ isOnline, avatar, name }) => {
     return (
         <li className={s.item}>
-            <span className={s.status}>{isOnline}</span>
+            <span className={s.status} style={{backgroundColor: isOnline ? 'green' : 'red'}}>{isOnline}</span>
             <img className={s.avatar} src={avatar} alt={name} width="48" />
             <p className={s.name}>{name}</p>
         </li>)
